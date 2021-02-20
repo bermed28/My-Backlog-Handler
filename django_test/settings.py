df56f63b99f10ea36bc3b15e878f46e2dcfefe05
angel.vazquez18 <angel.vazquez25@upr.edu>
2021-02-20 16:16:01 -0400

    Test

--- django_test/settings.py
+++ django_test/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "Django_DB",
+        'USER': "root",
+        'PASSWORD': "newrootpassword",
+        'HOST': "localhost",
+        'PORT': '3306',
     }
 }
 
