33913df2cfea950a96717873daf091d2e34d8ded
Fernando J. Bermúdez Medina <bermeddev@gmail.com>
2021-03-12 20:44:47 -0400

    Started API integration

---
+++ request.py
@@ -0,0 +1,33 @@
+import requests, rawgpy, json
+from requests.auth import HTTPBasicAuth
+
+# rawg = rawgpy.RAWG("User-Agent, My Backlog Handler, S.O. Studios")
+# rawg.login("bermedDev@gmail.com", "Fer28nan")
+# search = rawg.search("call-of-duty",num_results=100)
+#
+#
+# gtaDict = {}
+#
+#
+# for game in search:
+#
+#     game.populate()
+#
+#     p = game.platforms
+#
+#     for plat in p:
+#             if "PC" not in plat.name and "Call of Duty" in game.name:
+#                 if game.name not in gtaDict:
+#                     gtaDict[game.name] = [plat.name]
+#                 else:
+#                     gtaDict[game.name] += [plat.name]
+
+
+#
+# for game, platforms in gtaDict.items():
+#     print(game, "-", platforms)
+
+
+
+
+
