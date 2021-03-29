71723edccc8b9a64b3faec458afded6decb1797f
Kevin <kevin.lugo7@upr.edu>
2021-03-29 16:57:17 -0400

    Implemented the Library Model

--- index/models.py
+++ index/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from social_django import models as oauth_models
 
 # Create your models here.
 class PlayerAccount(models.Model):
@@ -11,29 +12,6 @@ class PlayerAccount(models.Model):
 
     def __str__(self):
         return self.user_name
-
-# class Genre_Model(models.Model):
-#     genre_id = models.IntegerField(primary_key=True)
-#     genre_name = models.CharField(max_length=128)
-#
-# class Developer_Model(models.Model):
-#     dev_id = models.IntegerField(primary_key=True)
-#     dev_name = models.CharField(max_length=128)
-#
-# class Image_Model(models.Model):
-#     img_id = models.IntegerField(primary_key=True)
-#     img_url = models.URLField(max_length=200)
-#
-# class Game_Model(models.Model):
-#     game_id = models.IntegerField(primary_key=True)
-#     game_title = models.CharField(max_length=128)
-#
-#     genre_id = models.ForeignKey(Genre_Model, on_delete=models.CASCADE)
-#     dev_id = models.ForeignKey(Developer_Model, on_delete=models.CASCADE)
-#     platforms = JSONField()
-#     img_id = models.ForeignKey(Image_Model, on_delete=models.CASCADE)
-
-
 
 class Genre_Model(models.Model):
     genre_id = models.IntegerField(primary_key=True)
@@ -59,3 +37,14 @@ class Game_Model(models.Model):
     img_id = models.ForeignKey(Image_Model, on_delete=models.CASCADE)
 
 
+
+
+class Library_Model(models.Model):
+    owner_id = models.ForeignKey(oauth_models.USER_MODEL, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game_Model, through='Library_Membership')
+
+class Library_Membership(models.Model):
+    game = models.ForeignKey(Game_Model, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library_Model, on_delete=models.CASCADE)
+    last_played = models.DateField()
+    is_finished = models.BooleanField(default=False)
