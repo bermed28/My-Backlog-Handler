c29ed218a7209447c47ba931be8d19f9a56bf589
angel.vazquez18 <angel.vazquez25@upr.edu>
2021-04-19 21:59:40 -0400

    Rating System

--- index/views.py
+++ index/views.py
@@ -3,44 +3,60 @@ from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from django.views.generic import ListView
 from django.db.models import Q
+from django.contrib import messages
 from igdb.wrapper import IGDBWrapper
 from .request import getSummary
 from .serializers import GameModelSerializer, ImageModelSerializer, DeveloperModelSerializer, GenreModelSerializer, \
-PlayerAccountSerializer,LibraryModelSerializer, LibraryMembershipSerializer
+    PlayerAccountSerializer, LibraryModelSerializer, LibraryMembershipSerializer, RatingModelSerializer
 
-from .models import Game_Model, PlayerAccount, Image_Model, Developer_Model, Genre_Model, Library_Model, Library_Membership
+from .models import Game_Model, PlayerAccount, Image_Model, Developer_Model, Genre_Model, Library_Model, \
+    Library_Membership, Ratings_Model
 
 """VIEWSETS"""
+
+
 class PlayerAccountViewSet(viewsets.ModelViewSet):
     queryset = PlayerAccount.objects.all().order_by('player_name')
     serializer_class = PlayerAccountSerializer
+
 
 class GameModelViewSet(viewsets.ModelViewSet):
     queryset = Game_Model.objects.all().order_by('game_id')
     serializer_class = GameModelSerializer
 
+
 class ImageModelViewSet(viewsets.ModelViewSet):
     queryset = Image_Model.objects.all().order_by('img_id')
     serializer_class = ImageModelSerializer
+
 
 class DeveloperModelViewSet(viewsets.ModelViewSet):
     queryset = Developer_Model.objects.all().order_by('dev_id')
     serializer_class = DeveloperModelSerializer
 
+
 class GenreModelViewSet(viewsets.ModelViewSet):
     queryset = Genre_Model.objects.all().order_by('genre_id')
     serializer_class = GenreModelSerializer
 
+
 class LibraryModelViewSet(viewsets.ModelViewSet):
     queryset = Library_Model.objects.all().order_by('owner_id')
     serializer_class = LibraryModelSerializer
+
 
 class LibraryMembershipViewSet(viewsets.ModelViewSet):
     queryset = Library_Membership.objects.all().order_by('library')
     serializer_class = LibraryMembershipSerializer
 
 
+class RatingModelViewSet(viewsets.ModelViewSet):
+    queryset = Ratings_Model.objects.all().order_by('overall_rating')
+    serializer_class = RatingModelSerializer
+
+
 """REDIRECTS"""
+
 
 class HomeGameView(ListView):
     model = Game_Model
@@ -50,6 +66,7 @@ class HomeGameView(ListView):
         game_model_list = Game_Model.objects
         return game_model_list
 
+
 class LibraryGameView(ListView):
     model = Game_Model
     template_name = '../templates/home/library.html'
@@ -57,6 +74,7 @@ class LibraryGameView(ListView):
     def get_list(self):
         game_model_list = Game_Model.objects
         return game_model_list
+
 
 class BacklogGameView(ListView):
     model = Game_Model
@@ -78,6 +96,7 @@ class SearchResultsGameView(ListView):
         )
         return game_model_list
 
+
 class SearchResultsImgView(ListView):
     model = Image_Model
     template_name = '../templates/home/search_results.html'
@@ -89,56 +108,79 @@ class SearchResultsImgView(ListView):
         )
         return image_model_ist
 
+
 def registration(request):
     return render(request, 'home/registration.html')
+
 
 def aboutUs(request):
     return render(request, 'home/about-us.html')
 
+
 def gameArticleTemplate(request, game_id):
     wrapper = IGDBWrapper("2zu4l0leu7rrc9i8ysagqlxuu5rh89", "r2raogtcmwho8ja4fv6b8si2h7u7ag")
     gameArticle = Game_Model.objects.get(game_id=game_id)
-    summary = getSummary(game_id,wrapper)
-    return render(request, 'home/game-article-template.html', {'gameArticle' : gameArticle, "gameSummary": summary})
+    summary = getSummary(game_id, wrapper)
+    if request.user.is_authenticated:
+        if request.method == 'POST' and request.POST.get('overall_rating'):
+            saveRating = Ratings_Model(user_id_id=request.user.id, game_id=game_id,
+                                       overall_rating=request.POST.get('overall_rating'))
+            saveRating.save()
+            messages.success(request, 'Rating Saved Successfully!')
+            return render(request, 'home/game-article-template.html',
+                          {'gameArticle': gameArticle, "gameSummary": summary})
+        else:
+            return render(request, 'home/game-article-template.html',
+                          {'gameArticle': gameArticle, "gameSummary": summary})
+    else:
+        messages.error(request, 'Log In To Have Access To Our Rating System')
+        return render(request, 'home/game-article-template.html',
+                      {'gameArticle': gameArticle, "gameSummary": summary})
 
 
 def tips(request):
     return render(request, 'home/tips.html')
 
+
 def newReleases(request):
     return render(request, 'home/new-releases.html')
+
 
 def popGames(request):
     return render(request, 'home/popular-games.html')
 
+
 def upGames(request):
     return render(request, 'home/upcoming-games.html')
+
 
 def settings(request):
     return render(request, 'home/settings.html')
 
+
 def wishlist(request):
     return render(request, 'home/wishlist.html')
+
 
 def profile(request):
     return render(request, 'home/profile.html')
 
+
 def favorites(request):
     return render(request, 'home/favorites.html')
+
 
 def fourOFour(request, exception):
     return render(request=request, template_name='home/errorHandling/404.html')
 
+
 def fiveHundred(request):
     return render(request=request, template_name='home/errorHandling/500.html')
+
 
 def fourHundred(request, exception):
     return render(request=request, template_name='home/errorHandling/400.html')
 
+
 def fourOThree(request, exception):
     return render(request=request, template_name='home/errorHandling/403.html')
-
-
-
-
-
--- register/forms.py
+++ register/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=120)
 
