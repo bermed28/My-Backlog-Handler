f94bebe6ebcab94dbd2973b7ac5b8ec71de50b9c
Kevin <kevin.lugo7@upr.edu>
2021-04-25 22:21:20 -0400

    Working Account Delete and register logged in redirect

--- register/views.py
+++ register/views.py
@@ -10,36 +10,25 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 # Create your views here.
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            messages.info(request, "Thanks for registering. You are now logged in.")
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],
-                                    )
-            login(request, new_user)
-            return HttpResponseRedirect("homepage")
+
+def register(response):
+    if response.user.is_authenticated:
+        return redirect("homepage")
 
     else:
-        form = RegisterForm(request.POST)
-        return render(request, "register/register.html", {"form": form})
+        if response.method == "POST":
+            form = RegisterForm(response.POST)
+            if form.is_valid():
+                new_user = form.save()
+                # messages.info(response, "Thanks for registering. You are now logged in.")
+                new_user = authenticate(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'],
+                                        )
+                login(response, new_user)
+            return redirect("homepage")
+        else:
+            form = RegisterForm()
 
-
-# def register(response):
-#     if response.user.is_authenticated:
-#         return redirect("homepage")
-#
-#     else:
-#         if response.method == "POST":
-#             form = RegisterForm(response.POST)
-#             if form.is_valid():
-#                 form.save()
-#             return redirect("homepage")
-#         else:
-#             form = RegisterForm()
-#
-#         return render(response, "register/register.html", {"form": form})
+        return render(response, "register/register.html", {"form": form})
 
 
